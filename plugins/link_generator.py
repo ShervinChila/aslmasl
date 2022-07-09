e913167d9b320f9dca7bf127718f8950517ea300
ShervinChila <108692453+ShervinChila@users.noreply.github.com>
2022-07-10 00:29:49 +0430

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,4 +1,4 @@
-#(©)Codexbotz
+#(©)CrowdShot
 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
@@ -10,51 +10,51 @@ from helper_func import encode, get_message_id
 async def batch(client: Client, message: Message):
     while True:
         try:
-            first_message = await client.ask(text = "Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            first_message = await client.ask(text = "بَچ نظر مثبتته؟☺️\n\n اوکی فایل ابتدایی مورد نظرتو از چنل <a href='https://t.me/decoupagechannel/'>دکـوپـاژ</a> برام بفرست", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         f_msg_id = await get_message_id(client, first_message)
         if f_msg_id:
             break
         else:
-            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            await first_message.reply("نظر مثبتت نظر منفیم شد😂\n\nاز چنل <a href='https://t.me/decoupagechannel/'>دکـوپـاژ</a> برام بفرست موش", quote = True)
             continue
 
     while True:
         try:
-            second_message = await client.ask(text = "Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            second_message = await client.ask(text = "اوکی☺️\n\nحالا آخرین فایل مورد نظرتو از چنل <a href='https://t.me/decoupagechannel/'>دکـوپـاژ</a> برام بفرست", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         s_msg_id = await get_message_id(client, second_message)
         if s_msg_id:
             break
         else:
-            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            await second_message.reply("نظر مثبتت نظر منفیم شد😂\n\nاز چنل <a href='https://t.me/decoupagechannel/'>دکـوپـاژ</a> برام بفرست موش", quote = True)
             continue
 
 
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("اشتراک گذاری", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>بفرما اینم لینک شما😻</b>\n\n{link}", quote=True, reply_markup=reply_markup)
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
-            channel_message = await client.ask(text = "Forward Message from the DB Channel (with Quotes)..\nor Send the DB Channel Post link", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            channel_message = await client.ask(text = "لینک نظر مثبتته؟☺️\n\nاوکی فایل مورد نظرتو از چنل <a href='https://t.me/decoupagechannel/'>دکـوپـاژ</a> برام بفرست", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except:
             return
         msg_id = await get_message_id(client, channel_message)
         if msg_id:
             break
         else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
+            await channel_message.reply("د نشد د😂\n\nاز چنل <a href='https://t.me/decoupagechannel/'>دکـوپـاژ</a> برام بفرست", quote = True)
             continue
 
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("اشتراک گذاری", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b>بفرما اینم لینک شما😻</b>\n\n{link}", quote=True, reply_markup=reply_markup)
